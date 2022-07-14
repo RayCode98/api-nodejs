cbb528ae0ba2c3c0480b3aedf5c90507ab7f166a
Jose Ramon Palacios Martinez <ramonp150398@gmail.com>
2022-07-14 15:05:11 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const cors = require('cors')
+
+const server = express()
+
+const logger = require('./loggerMiddleware')
+
+server.use(cors())
+
+server.use(express.json())
+
+server.use(logger)
+
+let datos = [
+  {
+    id: 1,
+    contenido: 'Hola mundo esto es una prueba',
+    importante: true
+  },
+  {
+    id: 2,
+    contenido: 'nota dos',
+    importante: true
+  }
+]
+
+server.get('/api/notes', (req, res) => {
+  res.json(datos)
+})
+
+server.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id
+  const nota = datos.find(nota => nota.id === id)
+  if (nota) {
+    res.json(nota)
+  } else {
+    res.json({
+      error: 'Nota no encontrada'
+    })
+  }
+})
+
+server.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id
+  const nota = datos.find(nota => nota.id === id)
+  if (nota) {
+    datos = datos.filter(nota => nota.id !== id)
+    res.json({
+      mensaje: 'Nota eliminada',
+      id
+    })
+  } else {
+    res.json({
+      error: 'Nota no encontrada'
+    })
+  }
+})
+
+server.post('/api/notes', (req, res) => {
+  const nota = req.body
+
+  if (!nota || !nota.contenido) {
+    res.status(400).json({
+      error: 'Falta contenido'
+    })
+  }
+
+  const id = datos.map(nota => nota.id)
+  const maxId = Math.max(...id)
+
+  const notaNueva = {
+    id: maxId + 1,
+    contenido: nota.contenido,
+    importante: nota.importante || false
+  }
+
+  datos = [...datos, notaNueva]
+  res.json(notaNueva)
+})
+
+// Erro 404
+server.use((req, res) => {
+  res.status(404).json({
+    error: 'Not found'
+  })
+})
+
+server.listen(3000, () => {
+  console.log('Servidor corriendo en el puerto 3000')
+})
